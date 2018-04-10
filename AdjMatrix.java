4f134075a9758e81a56346b9ba82a8974c21f18b
will <s3660470@student.rmit.edu.au>
2018-04-10 10:19:44 +1000

    array doubling

--- AdjMatrix.java
+++ AdjMatrix.java
@@ -12,19 +12,20 @@ import java.util.*;
 public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
     private static final int INITIAL_SIZE = 1000;
-    private static final int SIZE_INCREMENT = 1000;
+    private static final int SIZE_MULTIPLIER = 2;
 
     //Keep the total number of vertices (quicker than list length getters)
-    private int numVertices;
+    //FIXME decide on this
+    //private int numVertices;
     //Two maps because Java has no bi-directional map
     private Map<T, Integer> indexes; //Maps Vertices to their Indexes
     private Map<Integer, T> vertices; //Maps Indexes to their Vertices
     private boolean[][] matrix; //The actual Adjacency matrix
-	/**
-	 * Contructs empty graph.
-	 */
+    /**
+     * Contructs empty graph.
+     */
     public AdjMatrix() {
-        numVertices = 0;
+        //numVertices = 0;
         indexes = new HashMap<T, Integer>();
         vertices = new HashMap<Integer, T>();
         matrix = new boolean[INITIAL_SIZE][INITIAL_SIZE];
@@ -37,14 +38,15 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         int insert = getNextFreeSpot();
         //no free spot
         if (insert == -1) {
-            boolean[][] newMatrix = new boolean[numVertices+SIZE_INCREMENT][numVertices+SIZE_INCREMENT];
-            for (int i = 0; i < numVertices; i++) {
-                System.arraycopy(matrix[i], 0, newMatrix[i], 0, numVertices);
+            //double array size and copy over the current graph data
+            boolean[][] newMatrix = new boolean[matrix.length*SIZE_MULTIPLIER][matrix.length*SIZE_MULTIPLIER];
+            for (int i = 0; i < matrix.length; i++) {
+                System.arraycopy(matrix[i], 0, newMatrix[i], 0, matrix.length);
             }
+            //index the new vertex
+            indexes.put(vertLabel, matrix.length);
+            vertices.put(matrix.length, vertLabel);
             matrix = newMatrix;
-            indexes.put(vertLabel, numVertices);
-            vertices.put(numVertices, vertLabel);
-            numVertices += SIZE_INCREMENT;
         } else {
             //free spot
             indexes.put(vertLabel, insert);
@@ -60,19 +62,19 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         matrix[srcPos][tarPos] = true;
         matrix[tarPos][srcPos] = true;
     } // end of addEdge()
-	
+
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
         int vertIndex = indexes.get(vertLabel);
-        for (int i = 0; i < numVertices; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (matrix[vertIndex][i]) {
                 neighbours.add(vertices.get(i));
             }
         }
         return neighbours;
     } // end of neighbours()
-    
+
     public void removeVertex(T vertLabel) {
         if (!indexes.containsKey(vertLabel)) return;
         int removalIndex = indexes.get(vertLabel);
@@ -82,8 +84,8 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         indexes.remove(vertLabel);
         vertices.remove(removalIndex);
     } // end of removeVertex()
-	
-    
+
+
     public void removeEdge(T srcLabel, T tarLabel) {
         //validate edge before removing
         if (!indexes.containsKey(srcLabel) || !indexes.containsKey(tarLabel))
@@ -91,8 +93,8 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         matrix[indexes.get(srcLabel)][indexes.get(tarLabel)] = false;
         matrix[indexes.get(tarLabel)][indexes.get(srcLabel)] = false;
     } // end of removeEdges()
-	
-    
+
+
     public void printVertices(PrintWriter os) {
         for (Object v : indexes.keySet()) {
             os.print(v.toString() + " ");
@@ -100,11 +102,11 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         os.println();
         os.flush();
     } // end of printVertices()
-	
-    
+
+
     public void printEdges(PrintWriter os) {
-        for (int i = 0; i < numVertices; i++) {
-            for (int j = 0; j < numVertices; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
                 if (matrix[i][j]) {
                     T v1 = vertices.get(i);
                     T v2 = vertices.get(j);
@@ -114,13 +116,13 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
         }
         os.flush();
     } // end of printEdges()
-    
-    
+
+
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
         Queue<T> toVisit = new LinkedList<T>();
         Queue<Integer> depths = new LinkedList<Integer>();
-        boolean[] marked = new boolean[numVertices];
-        for (int i = 0; i < numVertices; i++) marked[i] = false;
+        boolean[] marked = new boolean[matrix.length];
+        for (int i = 0; i < matrix.length; i++) marked[i] = false;
         toVisit.add(vertLabel1);
         depths.add(0);
         while (!toVisit.isEmpty()) {
@@ -130,20 +132,18 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
             if (marked[indexes.get(v)]) continue;
             marked[indexes.get(v)] = true;
             for (T w : neighbours(v)) {
-                //if (!marked[indexes.get(w)]) {
-                    toVisit.add(w);
-                    depths.add(d+1);
-                //}
+                toVisit.add(w);
+                depths.add(d+1);
             }
         }
         return disconnectedDist;
     } // end of shortestPathDistance()
-    
+
 
     //helper function
     //returns the first array slot that has no vertex associated with it
     private int getNextFreeSpot() {
-        for (int i = 0; i < numVertices; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (!vertices.containsKey(i)) return i;
         }
         return -1;
