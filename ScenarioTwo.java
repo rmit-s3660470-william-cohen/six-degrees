65e44630a6c1a06fa69eb0bfe173894eafcbbfd3
will <s3660470@student.rmit.edu.au>
2018-04-14 18:15:38 +1000

    fixed scenario 2 bug

--- ScenarioTwo.java
+++ ScenarioTwo.java
@@ -1,12 +1,28 @@
 import java.io.*;
 import java.util.*;
 
+class Pair<T extends Object> {
+    T t1;
+    T t2;
+    public Pair(T t1, T t2) {
+        this.t1 = t1;
+        this.t2 = t2;
+    }
+    @Override
+    public boolean equals(Object o) {
+        //if ()
+        return false;
+    }
+
+}
+
 public class ScenarioTwo {
-    static final int NUMBER_OF_PAIRS = 500;
-    public static void main(String[] args) {
+    static final int NUMBER_OF_PAIRS = 100;
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println();
         }
+        System.out.println("Loading graph data...");
         String fileName = args[0];
         FriendshipGraph<String> adjgraph = new AdjMatrix<String>();
         FriendshipGraph<String> indgraph = new IndMatrix<String>();
@@ -36,7 +52,7 @@ public class ScenarioTwo {
         } catch (IOException ei) {
             System.err.println("Error: Something went wrong.");
         }
-
+        System.out.println("Generatinig pairs...");
         StringWriter vertexData = new StringWriter();
         adjgraph.printVertices(new PrintWriter(vertexData));
         String[] vertices = vertexData.toString().split(" ");
@@ -46,43 +62,28 @@ public class ScenarioTwo {
             String v1 = vertices[new Random().nextInt(sampleSize)];
             String v2 = vertices[new Random().nextInt(sampleSize)];
             TreeSet<String> pair = new TreeSet<String>();
-    	    pair.add(v1);
-    	    pair.add(v2);
+            pair.add(v1);
+            pair.add(v2);
             pairs.add(pair);
         }
-        long startTime, timeTaken;
-        double avgDist;
+        System.out.println("-- Testing with " + NUMBER_OF_PAIRS + "pairs --");
+        timeAvgDistance(adjgraph, pairs);
+        long time = timeAvgDistance(adjgraph, pairs);
+        System.out.println("AdjMatrix time: " + time);
 
-        System.out.println("Timing AdjMatrix...");
-        startTime = System.nanoTime();
-        avgDist = 0;
-        for (TreeSet<String> p : pairs) {
-            String v1 = p.first();
-            String v2 = p.last();
-            int dist = adjgraph.shortestPathDistance(v1,v2);
-            avgDist += (dist == -1 ? 0 : dist);
-        }
-        avgDist /= 100;
-        timeTaken = System.nanoTime()-startTime;
-        System.out.println("Avg. Distance: " + avgDist);
-        System.out.println("Time taken (ns): " + timeTaken);
-
-        System.out.println("Timing AdjMatrix again...");
-        startTime = System.nanoTime();
-        avgDist = 0;
-        for (TreeSet<String> p : pairs) {
-            String v1 = p.first();
-            String v2 = p.last();
-            int dist = adjgraph.shortestPathDistance(v1,v2);
-            avgDist += (dist == -1 ? 0 : dist);
-        }
-        avgDist /= 100;
-        timeTaken = System.nanoTime()-startTime;
-        System.out.println("Avg. Distance: " + avgDist);
-        System.out.println("Time taken (ns): " + timeTaken);
-
+        long time2 = timeAvgDistance(adjgraph, pairs);
+        System.out.println("IndMatrix time: " + time2);
     }
-    double avgDistance(FriendshipGraph<String> graph, Set<String> pairs) {
-
+    static long timeAvgDistance(FriendshipGraph<String> graph, Set<TreeSet<String>> pairs) {
+        long startTime = System.nanoTime();
+        double avgDist = 0;
+        for (TreeSet<String> p : pairs) {
+            String v1 = p.first();
+            String v2 = p.last();
+            int dist = graph.shortestPathDistance(v1,v2);
+            avgDist += (dist == -1 ? 0 : dist);
+        }
+        avgDist /= 100;
+        return System.nanoTime() - startTime;
     }
 }
