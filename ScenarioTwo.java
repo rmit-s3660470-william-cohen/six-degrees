ce1df5ae69ee82521ab3962e12f9870162e1aa40
will <s3660470@student.rmit.edu.au>
2018-04-14 15:15:02 +1000

    stringwriter

--- ScenarioTwo.java
+++ ScenarioTwo.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class ScenarioTwo {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println();
+        }
         String fileName = args[0];
         FriendshipGraph<String> adjgraph = new AdjMatrix<String>();
         FriendshipGraph<String> indgraph = new AdjMatrix<String>();
@@ -32,10 +35,17 @@ public class ScenarioTwo {
         } catch (IOException ei) {
             System.err.println("Error: Something went wrong.");
         }
-        StringWriter vertexData = new StringWriter();
-        graph.printVertices(new PrintWriter(vertexData));
-        String[] vertices = vertexData.toString().split(" ");
 
+        StringWriter vertexData = new StringWriter();
+        adjgraph.printVertices(new PrintWriter(vertexData));
+        String[] vertices = vertexData.toString().split(" ");
+        //for (String v : vertices) {
+          //  System.out.println(v);
+        //}
+        System.out.println("Total vertices: " + vertices.length);
+        
+        //TreeSet
+        //int edgeIndex = new Random().nextInt(edges.size());
 
     }
 }
